15cbc350d6a2d891ae74f605b989491e35e4c3f8
NaushinK <naushink2709@gmail.com>
2025-03-05 07:32:41 +0530

    update

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const  profileRouter = require('./src/routes/profile');
 const  requestRouter  = require('./src/routes/request');
 const userRouter= require('./src/routes/user')
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://famous-faloodeh-1b63c6.netlify.app",
   credentials:true
 }))
 app.use(express.json());
