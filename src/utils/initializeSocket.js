db19a816761d92decb353092d38ca2f4fe96f45b
NaushinK <naushink2709@gmail.com>
2025-03-20 00:47:24 +0530

    finally webscoket sompleted

--- src/utils/initializeSocket.js
+++ src/utils/initializeSocket.js
@@ -1,22 +1,42 @@
 const socket=require('socket.io')
+const crypto = require("crypto");
+const getSecretRoomId = (userId, targetUserId) => {
+    return crypto
+      .createHash("sha256")
+      .update([userId, targetUserId].sort().join("$"))
+      .digest("hex");
+  };
 
 const initializeSocket=(server)=>{
-   const io= socket (server,{
-    cors:{
-        origin:"http://localhost:5173"
-    }
-   })
+    console.log("🛠️ Initializing WebSocket server..."); 
+   
+    const io = socket(server, {
+        cors: {
+            origin: ["http://localhost:5173", "https://localhost:5173"], // Frontend URL
+    methods: ["GET", "POST"],
+    credentials: true,
+        },
+      });
+  
+      io.on("connection", (socket) => {
+       
+        console.log("✅ New WebSocket Connection:", socket.id);
+        socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+            if (!userId || !targetUserId) {
+                console.error("❌ Missing userId or targetUserId");
+                return;
+            }
+            const roomId=getSecretRoomId(userId,targetUserId)
+          console.log(firstName + " joined Room : " + roomId);
+          socket.join(roomId);
+        });
+      socket.on("sendMessage",({firstName, userId,targetUserId, text})=>{
+        const roomId=getSecretRoomId(userId,targetUserId)
+        console.log(firstName+""+text)
+        io.to(roomId).emit("messageRecieved",{firstName,text})
+      })
 
-   io.on("connection",(socket)=>{
-    socket.on("joinChat", ({ userId, targetUserId }) => {
-        if (!userId || !targetUserId) return;
-        const roomId = [userId, targetUserId].sort().join("_"); // Ensure consistency
-        console.log("User joined room:", roomId);
-        socket.join(roomId);
-    });
-    
-    socket.on("sendMessage",()=>{})
-    socket.on("disconnet",()=>{})
-   })
+    })
 }
-module.exports=initializeSocket
+
+module.exports = initializeSocket;
